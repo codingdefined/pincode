96fadbf3208ab4928df8e272ea9903674e0c1dac
Brandon Moody <brandon@thef33d.com>
2018-10-06 14:58:26 -0400

    coversion to typescript completed

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fast_csv_1 = __importDefault(require("fast-csv"));
-const pinCodeCsvPath = "./pinCode.csv";
+const path_1 = __importDefault(require("path"));
+const pinCodeCsvPath = path_1.default.join(__dirname, "../pinCode.csv");
 function pinCodeParser(csvLineData) {
     return {
         PinCode: csvLineData[1],
@@ -17,20 +18,77 @@ function pinCodeParser(csvLineData) {
     };
 }
 // TODO: define data in on "data" callback
-function searchByPin(pin, callback) {
-    const result = [];
+// TODO: accept numbers into searchByPin Function
+// TODO: convert functions to promises or async/await for readability
+function searchByPin(pin, cb) {
+    const results = [];
     if (pin.length !== 6) {
-        return callback("Invalid PinCode");
+        return cb("Invalid PinCode");
     }
-    const csvStream = fast_csv_1.default.fromPath(pinCodeCsvPath)
+    const csvStream = fast_csv_1.default.fromPath(pinCodeCsvPath);
+    csvStream
         .on("data", function (data) {
         if (data[1] === pin) {
-            result.push(pinCodeParser(data));
+            results.push(pinCodeParser(data));
         }
     })
         .on("end", function () {
-        return callback(result);
+        return cb(results);
     });
 }
 exports.searchByPin = searchByPin;
+function searchByArea(area, cb) {
+    const results = [];
+    const csvStream = fast_csv_1.default.fromPath(pinCodeCsvPath);
+    csvStream
+        .on("data", function (data) {
+        try {
+            if (data[0].indexOf(area) > -1) {
+                results.push(pinCodeParser(data));
+            }
+        }
+        catch (e) {
+        }
+    })
+        .on("end", function () {
+        return cb(results);
+    });
+}
+exports.searchByArea = searchByArea;
+function searchByDistrict(district, cb) {
+    const results = [];
+    const csvStream = fast_csv_1.default.fromPath(pinCodeCsvPath);
+    csvStream
+        .on("data", function (data) {
+        try {
+            if (data[8] === district) {
+                results.push(pinCodeParser(data));
+            }
+        }
+        catch (e) {
+        }
+    })
+        .on("end", function () {
+        return cb(results);
+    });
+}
+exports.searchByDistrict = searchByDistrict;
+function searchByTaluk(taluk, cb) {
+    const results = [];
+    const csvStream = fast_csv_1.default.fromPath(pinCodeCsvPath);
+    csvStream
+        .on("data", function (data) {
+        try {
+            if (data[7].indexOf(taluk) > -1) {
+                results.push(pinCodeParser(data));
+            }
+        }
+        catch (e) {
+        }
+    })
+        .on("end", function () {
+        return cb(results);
+    });
+}
+exports.searchByTaluk = searchByTaluk;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import * as csv from "fast-csv";
+import csv from "fast-csv";
 import fs from "fs";
 
+
+import path from "path";
+const pinCodeCsvPath = path.join(__dirname, "../pinCode.csv");
 
 
 interface PinResult {
@@ -13,7 +16,7 @@ interface PinResult {
   StateName: string;
 }
 
-const pinCodeCsvPath = "./pinCode.csv";
+
 
 function pinCodeParser(csvLineData: string[]): PinResult {
   return {
@@ -28,117 +31,82 @@ function pinCodeParser(csvLineData: string[]): PinResult {
 }
 
 // TODO: define data in on "data" callback
-function searchByPin(pin: string, callback: Function) {
-  const result: PinResult[] = [];
+// TODO: accept numbers into searchByPin Function
+// TODO: convert functions to promises or async/await for readability
+
+function searchByPin(pin: string, cb: Function) {
+  const results: PinResult[] = [];
   if (pin.length !== 6) {
-    return callback("Invalid PinCode");
+    return cb("Invalid PinCode");
   }
-  const csvStream = csv.fromPath(pinCodeCsvPath)
-                        .on("data",
-                            function(data: any) {
-                              if (data[1] === pin) {
-                                result.push(pinCodeParser(data));
-                              }
-                            })
-                        .on("end", function() {
-                          return callback(result);
-                        });
+  const csvStream = csv.fromPath(pinCodeCsvPath);
+  csvStream
+      .on("data",
+          function(data: any) {
+            if (data[1] === pin) {
+              results.push(pinCodeParser(data));
+            }
+          })
+      .on("end", function() {
+        return cb(results);
+      });
 }
 
 
-// exports.seachByPin = function(pin, callback) {
-//   if (pin.length < 6 || pin.length > 6) return callback("Invalid PinCode");
-//   const csvStream = csv.fromPath("pinCode.csv")
-//                         .on("data",
-//                             function(data) {
-//                               if (data[1] === pin) {
-//                                 result.push({
-//                                   PinCode: data[1],
-//                                   OfficeName: data[0],
-//                                   DivisionName: data[4],
-//                                   RigionName: data[5],
-//                                   Taluk: data[7],
-//                                   DistrictName: data[8],
-//                                   StateName: data[9]
-//                                 });
-//                               }
-//                             })
-//                         .on("end", function() {
-//                           return callback(result);
-//                         });
-// };
-
-// exports.seachByArea = function(area, callback) {
-//   const csvStream = csv.fromPath("pinCode.csv")
-//                         .on("data",
-//                             function(data) {
-//                               try {
-//                                 if (data[0].indexOf(area) > -1) {
-//                                   result.push({
-//                                     PinCode: data[1],
-//                                     OfficeName: data[0],
-//                                     DivisionName: data[4],
-//                                     RigionName: data[5],
-//                                     Taluk: data[7],
-//                                     DistrictName: data[8],
-//                                     StateName: data[9]
-//                                   });
-//                                 }
-//                               } catch (e) {
-//                               }
-//                             })
-//                         .on("end", function() {
-//                           return callback(result);
-//                         });
-// };
-
-// exports.seachByDistrict = function(district, callback) {
-//   const csvStream = csv.fromPath("pinCode.csv")
-//                         .on("data",
-//                             function(data) {
-//                               try {
-//                                 if (data[8] === district) {
-//                                   result.push({
-//                                     PinCode: data[1],
-//                                     OfficeName: data[0],
-//                                     DivisionName: data[4],
-//                                     RigionName: data[5],
-//                                     Taluk: data[7],
-//                                     DistrictName: data[8],
-//                                     StateName: data[9]
-//                                   });
-//                                 }
-//                               } catch (e) {
-//                               }
-//                             })
-//                         .on("end", function() {
-//                           return callback(result);
-//                         });
-// };
-
-// exports.seachByTaluk = function(taluk, callback) {
-//   const csvStream = csv.fromPath("pinCode.csv")
-//                         .on("data",
-//                             function(data) {
-//                               try {
-//                                 if (data[7].indexOf(taluk) > -1) {
-//                                   result.push({
-//                                     PinCode: data[1],
-//                                     OfficeName: data[0],
-//                                     DivisionName: data[4],
-//                                     RigionName: data[5],
-//                                     Taluk: data[7],
-//                                     DistrictName: data[8],
-//                                     StateName: data[9]
-//                                   });
-//                                 }
-//                               } catch (e) {
-//                               }
-//                             })
-//                         .on("end", function() {
-//                           return callback(result);
-//                         });
-// };
+function searchByArea(area: string, cb: Function) {
+  const results: PinResult[] = [];
+  const csvStream = csv.fromPath(pinCodeCsvPath);
+  csvStream
+      .on("data",
+          function(data) {
+            try {
+              if (data[0].indexOf(area) > -1) {
+                results.push(pinCodeParser(data));
+              }
+            } catch (e) {
+            }
+          })
+      .on("end", function() {
+        return cb(results);
+      });
+}
 
 
-export {searchByPin};
+function searchByDistrict(district: string, cb: Function) {
+  const results: PinResult[] = [];
+  const csvStream = csv.fromPath(pinCodeCsvPath);
+  csvStream
+      .on("data",
+          function(data) {
+            try {
+              if (data[8] === district) {
+                results.push(pinCodeParser(data));
+              }
+            } catch (e) {
+            }
+          })
+      .on("end", function() {
+        return cb(results);
+      });
+}
+
+function searchByTaluk(taluk: string, cb: Function) {
+  const results: PinResult[] = [];
+  const csvStream = csv.fromPath(pinCodeCsvPath);
+  csvStream
+      .on("data",
+          function(data) {
+            try {
+              if (data[7].indexOf(taluk) > -1) {
+                results.push(pinCodeParser(data));
+              }
+            } catch (e) {
+            }
+          })
+      .on("end", function() {
+        return cb(results);
+      });
+}
+
+
+export {searchByPin, searchByArea, searchByDistrict, searchByTaluk};
